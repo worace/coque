5e5f6c2cc5325b6cfe485a7c58cfa6d088df1fcd
Horace Williams <horace.d.williams@gmail.com>
2018-05-03 21:11:24 -0700

    Check buffering to array

--- test/sluice_test.rb
+++ test/sluice_test.rb
@@ -14,4 +14,11 @@ describe Sluice do
     assert(res.include?("Rakefile"))
     assert(res.pid > 0)
   end
+
+  it "can buffer to array" do
+    res = Sluice::Cmd["ls"].run.to_a
+    assert_equal(13, res.count)
+    # Can check a second time as result is cached
+    assert_equal(13, res.count)
+  end
 end
